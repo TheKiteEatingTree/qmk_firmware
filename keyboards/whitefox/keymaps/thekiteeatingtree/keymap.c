ab2e3c3cc1907cbc760078de55a00b36fd185488
Andrew Stephan <thekiteeatingtree@gmail.com>
2018-08-31 13:28:14 -0400

    updated whitefox keymap

--- keyboards/whitefox/keymaps/thekiteeatingtree/keymap.c
+++ keyboards/whitefox/keymaps/thekiteeatingtree/keymap.c
@@ -40,7 +40,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,   KC_BSLS,  KC_DEL, \
         LT_1_LGUI, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,  LT(2,KC_ENT),    KC_PGUP,\
         KC_LSFT,        KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,    KC_RSFT, KC_UP,  KC_PGDN,\
-        KC_LCTL,        KC_LALT,MO(2),                        KC_SPC,                 MO(3),          KC_RCTL,KC_NO,KC_LEFT,KC_DOWN,KC_RGHT \
+        KC_LCTL,        KC_LALT,KC_LGUI,KC_SPC,                 MO(3),          KC_RCTL,KC_NO,KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /* Layer 1: FN Layer
      * ,---------------------------------------------------------------------.
@@ -96,7 +96,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------'
      */
     [3] = LAYOUT( \
-        RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_MUTE,KC_VOLD,KC_VOLU,KC_TRNS,KC_TRNS,KC_TRNS,\
+        RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_MUTE,KC_VOLD,KC_VOLU,KC_TRNS,KC_TRNS,KC_PSCREEN,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MPLY,KC_MPRV,KC_MNXT,KC_TRNS,        BL_TOGG,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        BL_INC,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_TRNS,BL_DEC,\
